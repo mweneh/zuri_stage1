47f120a1531e32f6d623ffe8c2cb7ba7e53733a7
David <davidongaroh@gmail.com>
2023-09-10 15:21:06 +0300

    run python manage.py startapp + add app

--- zuri_stage1/settings.py
+++ zuri_stage1/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'stage1',
+    'SLACKS',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
